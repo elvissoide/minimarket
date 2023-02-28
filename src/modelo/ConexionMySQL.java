8881f5ce6cbf0315369c058d6a52eda16b84d78d
Elvis Guanoluisa <elviserock@gmail.com>
2023-02-27 20:03:27 -0500

    commit inicial

---
+++ src/modelo/ConexionMySQL.java
@@ -0,0 +1,22 @@
+package modelo;
+
+import javax.swing.*;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+public class ConexionMySQL {
+    Connection conexion;
+    public Connection crearConexion(){
+        String url = "jdbc:mysql://localhost:3306/minimarket";
+        String usuario = "root";
+        String contrasena = "Gatito1011";
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            conexion = DriverManager.getConnection(url, usuario, contrasena);
+        } catch (Exception e) {
+            JOptionPane.showMessageDialog(null,"ERROR DE CONEXION CON LA BASE DE DATOS");
+            e.printStackTrace();
+        }
+        return conexion;
+    }
+}
