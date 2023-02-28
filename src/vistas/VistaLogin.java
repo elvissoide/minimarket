1744ffe29048aca87965a78545a81b459f49564d
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-02-27 21:53:40 -0500

    interfaz panel login y interfaz panel principal del administrador

--- src/vistas/VistaLogin.java
+++ src/vistas/VistaLogin.java
@@ -1,4 +1,12 @@
 package vistas;
 
+import javax.swing.*;
+
 public class VistaLogin {
+    private JComboBox modocomboBox;
+    private JTextField usuarioTextField;
+    private JPasswordField passwordField;
+    private JButton ingresarButton;
+    private JButton salirButton;
+    private JPanel panelLogin;
 }
--- src/vistas/VistaPrincipalAdmi.java
+++ src/vistas/VistaPrincipalAdmi.java
@@ -1,4 +1,12 @@
 package vistas;
 
+import javax.swing.*;
+
 public class VistaPrincipalAdmi {
+    private JPanel panelPrincipalAdmi;
+    private JLabel NombreAdministrador;
+    private JLabel CorreoAdministrador;
+    private JButton ingresarProductosButton;
+    private JButton revisarVentasButton;
+    private JButton agregarCajeroButton;
 }
