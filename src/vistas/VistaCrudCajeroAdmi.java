ac475db2acd570ee56f522e0cd362c5412272c25
Shiao-Li <roberto.shiao@epn.edu.ec>
2023-03-01 10:46:09 -0500

    Interfaz gestion cajeros

--- src/vistas/VistaCrudCajeroAdmi.java
+++ src/vistas/VistaCrudCajeroAdmi.java
@@ -1,4 +1,18 @@
 package vistas;
 
+import javax.swing.*;
+
 public class VistaCrudCajeroAdmi {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JTable table1;
+    private JButton guardarButton;
+    private JButton listarButton;
+    private JButton seleccionarButton;
+    private JButton actualizarButton;
+    private JButton eliminarButton;
 }
