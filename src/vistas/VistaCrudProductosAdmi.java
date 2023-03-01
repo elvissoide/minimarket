8847bdfc67167ebf23df2c7f40aee4072c245be6
AlexisChasi <alexis.chasi@epn.edu.ec>
2023-03-01 10:48:16 -0500

    vistas gestion productos

--- src/vistas/VistaCrudProductosAdmi.java
+++ src/vistas/VistaCrudProductosAdmi.java
@@ -1,4 +1,21 @@
 package vistas;
 
+import javax.swing.*;
+
 public class VistaCrudProductosAdmi {
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JTextField textField4;
+    private JTextField textField5;
+    private JTextField textField6;
+    private JRadioButton SIRadioButton;
+    private JRadioButton NORadioButton;
+    private JTable informacionProductos;
+    private JButton IngresarButton;
+    private JButton actualizarButton;
+    private JButton eliminarButton;
+    private JButton guardarButton;
+    private JButton seleccionarButton;
+
 }
